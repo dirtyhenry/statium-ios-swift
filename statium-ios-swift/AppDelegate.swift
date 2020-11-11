02b02910739561eb119dd40ffa1c88b549d9007b
Mick F <mick@statium.io>
2020-11-11 18:20:44 +0100

    fix: prevent runtime error
    UIWindows were created prior to initial application activation. This may result in incorrect visual
    appearance.

--- statium-ios-swift/AppDelegate.swift
+++ statium-ios-swift/AppDelegate.swift
@@ -4,13 +4,13 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    let window = UIWindow(frame: UIScreen.main.bounds)
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
         -> Bool {
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
